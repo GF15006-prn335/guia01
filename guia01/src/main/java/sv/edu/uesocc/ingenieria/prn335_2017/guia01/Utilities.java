c124ffd3e616a367c983bb1f4cf909292da0c22d
PC17 <PC17@DESKTOP-G1SGP23>
2017-08-17 12:17:33 -0500

    metodos terminados

--- guia01/src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
+++ guia01/src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
@@ -10,14 +10,39 @@ package sv.edu.uesocc.ingenieria.prn335_2017.guia01;
  * @author PC17
  */
 public class Utilities {
+    /**
+     * mediante substring me recorre el rango asignado y lo almaceno en la variable cadena
+     * @param texto
+     * @return cadena
+     */
          public String getResume(String texto){
        String cadena=texto.substring(0, 29);
         return cadena;
     }
     
     public String capitalizar(String texto){
-        String cap=texto.replaceAll("  ", "");
-        return cap;
-        
+        String cap="";
+        texto.replaceAll("  ", " ");
+        cap=String.valueOf(texto.charAt(0)).toUpperCase();    
+        for(int i=0;1<texto.length();i++){
+            if(texto.charAt(i-1)==' ' || texto.charAt(i)==' '){
+                cap=String.valueOf(texto.charAt(i)).toUpperCase();
+            }else{
+                cap+=texto.charAt(i);
+            }
+        }
+             return cap;
+       
     }
+    public int contarCoincidencias(String frase, String texto){
+        int coincidencia=0;      
+   int posicion = texto.indexOf(frase);
+   while (  posicion != -1 ) {
+   coincidencia++;
+   posicion = texto.indexOf(frase,posicion+1);
+}
+      return coincidencia;
+    }
+    
+    
 }
